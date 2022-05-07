df2e84821686a5f0f1e281e68de2a8b316322c56
dpotekhin <d.potehin@peppers-studio.ru>
2022-05-07 14:43:10 +0300

    feat: added an option to remove clip-masks

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-
-
 const fs = require('fs-extra')
 const utils = require("./utils.js");
 const lottieToSvg = require("./lottieToSvg.js");
 const Path = require('path');
+const xml2js = require('xml2js');
 
+
+//
 var args = process.argv.slice(2);
 
 const src = args[0]; // 'Comp.json'
 const dest = args[1]; // `myanim_`;
 const startFrame = args[2] || 0;
-const endFrame = args[3] || true;
+let endFrame = args[3] || true;
+if( endFrame.toLowerCase() === 'true' ) endFrame = true;
+const removeClippingMasks = args[4];
 
 console.log('PARAMS:');
 console.log('1) source path*:', src );
 console.log('2) destination path*:', dest );
 console.log('3) start frame:', startFrame );
 console.log('4) end frame:', endFrame );
+console.log('5) remove clipping masks:', removeClippingMasks );
 console.log('');
 
 if( !src || !dest ){
   console.log('Error: a source path and a destination path are required.');
   process.exit(1);
 }
+
+const parser = new xml2js.Parser();
+const builder = new xml2js.Builder();
 
 const animationData = JSON.parse(fs.readFileSync( src, "utf8" ));
 
@@ -33,15 +40,93 @@ const init = async () => {
   await fs.ensureDir(destDir);
 
   console.log('Extracting frames...');
-  const frames = await lottieToSvg(animationData, startFrame, endFrame );
+  const frames = await lottieToSvg( animationData, startFrame, endFrame );
   console.log('Extracting complete:', frames.length );
 
   console.log('Writing the frames to SVG files...');
+
   utils.processArray( frames, async (frameData) => {
+
+    if( removeClippingMasks ){
+      frameData.data = await cleanupSVG( frameData.data );
+    }
+
     fs.writeFileSync(dest+`_${frameData.frame}.svg`, frameData.data, "utf8");
+
   });
   
   console.log('Complete');
+
+}
+
+const cleanupSVG = async ( data ) => {
+  
+  const jObj = await parser.parseStringPromise(data);
+  // console.log( jObj.svg.g );
+
+
+  // Remove clipping masks defenitions
+  if( jObj.svg.defs && jObj.svg.defs[0].clipPath ){
+    /*
+    jObj.svg.defs[0].clipPath.forEach(function(v,i){
+      console.log(i+') ',v);
+    });
+    */
+    delete jObj.svg.defs[0].clipPath;
+
+  }
+
+  removeClippingMasks( jObj.svg.g );
+
+  return builder.buildObject(jObj);
+  return JSON.stringify(jObj,true,'  '); // !!!
+  return data; // !!!
+
+  //
+  function removeClippingMasks( nd ){
+    
+    if( !nd || !nd.length ) {
+      // console.log('???', nd);      
+      return nd;
+    }
+
+    // console.log('>>>', nd);
+
+    const ndt = [];
+    let maskFound = false;
+
+    nd.forEach(function(_nd,i){
+
+      // console.log('>',_nd);
+
+      if( _nd.$ && _nd.$['clip-path'] ){
+
+        // console.log('MASK!');
+        maskFound = true;
+        if( _nd.g ) _nd.g.forEach( function(v) {
+          ndt.push( v );
+        });
+
+      }else{
+        // console.log('OTHER');
+        ndt.push( _nd );
+      }
+
+      if( !maskFound ) removeClippingMasks(_nd.g);
+
+    });
+
+    nd.length = 0;
+    // removeClippingMasks(ndt);
+    ndt.forEach(function(v) {
+      nd.push(v);
+    });
+
+    if( maskFound ) removeClippingMasks(nd);
+
+    return nd;
+
+  }
 
 }
 
